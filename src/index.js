9c88f81b16c6e64554b3fe74f273816717158f31
Ingamha <ingamha@gmail.com>
2025-05-07 16:06:47 +0200

    returning invalid api-key

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "1a6546fbd4afe78fc90o08t2850b2e3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
