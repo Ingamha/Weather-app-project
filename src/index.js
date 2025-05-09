88b1cbfde445d3338f838a87649187761cb211ec
Ingamha <ingamha@gmail.com>
2025-05-09 12:14:52 +0200

    Added search function with API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
-  let apiKey = "1a6546fbd4afe78fc90o08t2850b2e3";
+  let apiKey = "1a6546fbd4afe78fc90o08t2850b2e3e";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Oslo");
